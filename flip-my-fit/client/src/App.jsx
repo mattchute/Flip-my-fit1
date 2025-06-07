67de90f9b072667355b589863cbab9504a780da1
mattchute <matt@chute.media>
2025-06-06 20:16:16 -0400

    Delete flip-my-fit/client/src directory

--- flip-my-fit/client/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center text-center p-10">
-      <h1 className="text-3xl font-bold">Flip My Fit is live!</h1>
-    </div>
-  );
-}
